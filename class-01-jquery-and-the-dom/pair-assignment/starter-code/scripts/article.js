aa8121f4a67b587aa3ab63548e884a7ccd450eeb
Doug Popadince <gsmatth@icloud.com>
2016-01-05 19:24:31 -0800

    updated js and html to add functionality required by all todos

--- class-01-jquery-and-the-dom/pair-assignment/starter-code/scripts/article.js
+++ class-01-jquery-and-the-dom/pair-assignment/starter-code/scripts/article.js
@@ -1,42 +1,50 @@
 var articles = [];
 
 function Article (opts) {
-  // TODO: Use the js object passed in to complete this contructor function:
-  // Save ALL the properties of `opts` into `this`.
-  this.author = opts.author;
+  this.title = opts.title;   //'Bacon Ipsum',
+  this.category =  opts.category; //   'food',
+  this.author = opts.author; //Kevin Bacon
+  this.authorUrl = opts.authorUrl;  // 'http://www.imdb.com/name/nm0000102/',
+  this.publishedOn = opts.publishedOn; //'2015-11-05',
+  this.body = opts.body; //'<p>Bacon ipsum dolor amet do ex andouille minim,
 }
-
+//convert article object to html before inseerting it into page
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
 
-  $newArticle.data('category', this.category);
-
-  // TODO: Use jQuery to fill in the template with properties
-  // from this particular Article instance. We need to fill in:
-  // the author name and url, the article title and body, and the
-  // publication date.
+  $newArticle.find('h1').text(this.title);
+  $newArticle.find('a').text(this.author);
+  $newArticle.find('a').attr('href', this.authorUrl);
+  $newArticle.find('.article-body').html(this.body);
 
   // Include the publication date as a 'title' attribute to show on hover:
-  $newArticle.find('time[pubdate]').attr('title', this.publishedOn)
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
 
   // Display the date as a relative number of "days ago":
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
 
   $newArticle.append('<hr>');
 
-  // TODO: This cloned article is no longer a template, so we should remove that class...
-
+  $newArticle.removeClass('template');
   return $newArticle;
 }
+//-------------------end of converting to html-------------------------//
 
+
+/*sort array using a compare function.  If b is greater than a, a positive value is returned
+making b an newer article.  If a is greater than b, a negative number is returned
+making b an older article */
 rawData.sort(function(a,b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
+/*for each index/object in the rawData array, construct a new article object and
+push it into the articles array*/
 rawData.forEach(function(ele) {
   articles.push(new Article(ele));
-})
-
+});
+//for each index in the articles array and append the article to the end
+// of the article section
 articles.forEach(function(a){
   $('#articles').append(a.toHtml())
 });
