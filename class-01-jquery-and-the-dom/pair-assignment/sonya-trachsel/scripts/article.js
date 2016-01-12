9ba8df0a1c297c796e6807ee588bef09a9654be8
Sonya Trachsel <sofyatrachsel@gmail.com>
2016-01-12 08:59:21 -0800

    TODO's done by Sonya

---
+++ class-01-jquery-and-the-dom/pair-assignment/sonya-trachsel/scripts/article.js
@@ -0,0 +1,49 @@
+var articles = [];
+
+function Article (opts) {
+  // DONE: Use the js object passed in to complete this contructor function:
+  // Save ALL the properties of `opts` into `this`.
+  this.title = opts.title;
+  this.category = opts.category;
+  this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
+}
+
+Article.prototype.toHtml = function() {
+  var $newArticle = $('article.template').clone();
+
+  $newArticle.attr('data-category', this.category);
+
+  // DONE: Use jQuery to fill in the template with properties
+  // from this particular Article instance. We need to fill in:
+  // the author name and url, the article title and body, and the
+  // publication date.
+  $newArticle.find('h1').html(this.title)
+  $newArticle.find('address').html(this.author)
+  $newArticle.find('article-body').html(this.body)
+  // Include the publication date as a 'title' attribute to show on hover:
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn)
+
+  // Display the date as a relative number of "days ago":
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
+
+  $newArticle.append('<hr>');
+
+  // DONE: This cloned article is no longer a template, so we should remove that class...
+  $newArticle.removeClass("template");
+  return $newArticle;
+};
+
+rawData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+rawData.forEach(function(ele) {
+  articles.push(new Article(ele));
+})
+debugger;
+articles.forEach(function(a){
+  $('#articles').append(a.toHtml())
+});
