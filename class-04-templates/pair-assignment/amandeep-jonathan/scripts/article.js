bc553d1d20649cba184968aed72406c80dae0dbe
Randallonius <jonathanprandall@gmail.com>
2016-01-11 21:43:47 -0800

    added published time

--- class-04-templates/pair-assignment/amandeep-jonathan/scripts/article.js
+++ class-04-templates/pair-assignment/amandeep-jonathan/scripts/article.js
@@ -16,14 +16,14 @@ Article.prototype.toHtml = function() {
 
   var appTemplate = $('#hello-hi-hey').html();
   var compiledTemplate = Handlebars.compile(appTemplate);
-  var html = compiledTemplate(this);
+
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
   //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-
+  var html = compiledTemplate(this);
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
   return html;
 };
