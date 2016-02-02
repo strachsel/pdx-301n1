5786bda70afe61202e6e0c54b871c266b8f53585
Allison Davis <a.rae.davis@gmail.com>
2016-02-01 20:21:23 -0800

    add routes, finish article and about controllers

--- class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
@@ -1,4 +1,9 @@
 // TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
 
-// TODO: What function do you call to activate page.js? Fire it off now, to execute
+page('/', articlesController.index);
+
+page('/about', aboutController.index);
+
+// DONE: What function do you call to activate page.js? Fire it off now, to execute
+page();
